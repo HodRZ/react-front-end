d28debbd668ae6f914722287d389bff06d2f1807
HodRZ <hodaifa.radwan@gmail.com>
2022-09-04 22:03:38 +0300

    Add components WIP

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Person from './components/Person';
+
+function App() {
+  return (
+    <div className="App h-screen bg-zinc-300">
+      <Person />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Person.jsx
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+
+function Person() {
+    const [name, setName] = useState('Hod');
+    const [age, setAge] = useState('5');
+    const [gender, setGender] = useState('Bi-cycle')
+
+    function handleChangeName(e) {
+        setName(e.target.value)
+    }
+    function handleChangeAge(e) {
+        setAge(e.target.value)
+    }
+    function handleChangeGender(e) {
+        setGender(e.target.value)
+    }
+    async function handleSubmit(e) {
+        e.preventDefault()
+        const data = {
+            "name": e.target.name.value,
+            "age": e.target.age.value,
+            "gender": e.target.gender.value,
+        }
+        const newAge = await axios.post('http://localhost:3001/person', data).catch(function (error) { console.log(error) })
+        setAge(newAge.data)
+    }
+
+    return (
+        <div className=''>
+            <form onSubmit={handleSubmit} method="post" className='flex flex-col place-self-center'>
+                <h2>My Name is {name}</h2>
+                <input type="text" id='name' onChange={handleChangeName} />
+                <h2>My Age is {age}</h2>
+                <input type="number" id='age' onChange={handleChangeAge} />
+                <h2>My Name is {gender}</h2>
+                <input type="text" id='gender' onChange={handleChangeGender} />
+                <button >Click Me</button>
+            </form>
+
+        </div>
+    );
+}
+
+export default Person;
